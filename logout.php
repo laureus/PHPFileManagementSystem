51996af8edccd544817f5b61197c2cbae5f79de0
Jimmy Luo <laureus31@126.com>
2013-05-12 17:33:58 +1000

    add logout bar

--- logout.php
+++ logout.php
@@ -1,2 +1,5 @@
-<?php include "res/db.php"; $_SESSION = array(); session_destroy(); ?>
+<?php
+session_start();
+//include "res/db.php";
+$_SESSION = array(); session_destroy(); ?>
 <meta http-equiv="refresh" content="0;url=index.php">
