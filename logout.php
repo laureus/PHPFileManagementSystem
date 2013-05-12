658f062ed752b09dcd9a8272dece1158eaf67558
Jimmy Luo <laureus31@126.com>
2013-05-12 17:39:37 +1000

    add logout and login status bar

--- logout.php
+++ logout.php
@@ -1,2 +1,5 @@
-<?php include "res/db.php"; $_SESSION = array(); session_destroy(); ?>
+<?php 
+//include "res/db.php";
+session_start();
+$_SESSION = array(); session_destroy(); ?>
 <meta http-equiv="refresh" content="0;url=index.php">
